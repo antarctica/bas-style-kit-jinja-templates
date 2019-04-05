dcf4fb4a46560fe7a3d016bf85832cd36cf29bd2
Felix Fennell <felix@felixfennell.co.uk>
2019-04-05 10:26:43 +0100

    Adding HTML layout

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.do')
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.j2')
+@app.route('/basic')
+def basic_layout():
+    return render_template('basic.j2')
+
+
+@app.route('/html')
+def html_layout():
+    return render_template('html.j2')
