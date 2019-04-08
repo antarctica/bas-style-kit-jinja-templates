e90b44f7c4fdc0b1b86d8c0381b7f52dcbddeb16
Felix Fennell <felix@felixfennell.co.uk>
2019-04-08 08:19:48 +0100

    Refactoring templates
    Moving atributes to a config object rather than embedding within templates. Moving templates into a proper namespace.
    
    Templates loaded using a prefix and package template loader. Layout tests loaded dynamically rather than using specific routes.

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+# noinspection PyPackageRequirements
+from jinja2 import PrefixLoader, PackageLoader, FileSystemLoader
+
+from bas_style_kit_jinja_templates import BskTemplates
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.do')
+app.jinja_loader = PrefixLoader({
+    'app': FileSystemLoader('templates'),
+    'bas_style_kit': PackageLoader('bas_style_kit_jinja_templates'),
+})
+app.config['bsk_templates'] = BskTemplates()
+app.config['bsk_templates'].site_title = 'BAS Style Kit Jinja Templates'
+app.config['bsk_templates'].site_description = 'A set of Jinja templates implementing the BAS Style Kit'
 
 
-@app.route('/basic')
-def basic_layout():
-    return render_template('basic.j2')
+@app.route('/layouts/<layout>')
+def render_layout(layout: str):
+    layouts = [
+        'blank',
+        'html',
+    ]
+    if layout not in layouts:
+        abort(404)
+
+    # noinspection PyUnresolvedReferences
+    return render_template(f"app/layouts/{ layout }.j2")
 
 
-@app.route('/html')
-def html_layout():
-    return render_template('html.j2')
+
---
+++ bas_style_kit_jinja_templates/__init__.py
@@ -0,0 +1,15 @@
+class BskTemplates(object):
+    templates_version = '0.1.0-develop'
+    bsk_version = '0.5.0'
+
+    def __init__(self):
+        self.site_title = 'site title'
+        self.site_description = 'site description'
+        self.site_favicon = 'default'
+        self.site_back_to_top_target_id = 'site-top'
+        self.site_main_content_target_id = 'site-main-content'
+        self.site_styles = []
+        self.site_scripts = []
+        self.site_analytics = {}
+        self.container_classes = []
+        self.main_content_classes = []
