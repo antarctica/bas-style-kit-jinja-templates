9d9ae522cf5695960305ac2a780e1ba1835fc222
Felix Fennell <felnne@bas.ac.uk>
2021-03-17 10:50:08 +0000

    Updating to jQuery 0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Added
 
 * Updated Python dependencies
+* Updated to jQuery 3.6.0
 
 ## [0.5.0] - 2020-06-30
 
--- bas_style_kit_jinja_templates/__init__.py
+++ bas_style_kit_jinja_templates/__init__.py
@@ -22,8 +22,8 @@ class BskTemplates(object):
         ]
         self.bsk_scripts = [
             {
-                "href": f"https://cdn.web.bas.ac.uk/libs/jquery/3.5.1/js/jquery-3.5.1.min.js",
-                "integrity": "sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=",
+                "href": f"https://cdn.web.bas.ac.uk/libs/jquery/3.6.0/js/jquery-3.6.0.min.js",
+                "integrity": "sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=",
             },
             {
                 "href": f"https://cdn.web.bas.ac.uk/libs/js-cookie/2.2.1/js/js.cookie-2.2.1.min.js",
